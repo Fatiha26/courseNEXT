e79cd53b571fd044ed94612ba04828dc9fdb0a5c
Fatiha26 <f6lamia@gmail.com>
2024-10-11 12:15:30 +0600

    starting navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex py-3 flex-wrap justify-around'>
+      <h1 className='text-lg font-semibold'>Todo App</h1>
+
+    </div>
+  )
+}
+
+export default Navbar
