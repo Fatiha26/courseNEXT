8f69f084f3f5cdd8e547151488a2b1c1597c7157
Fatiha26 <f6lamia@gmail.com>
2024-10-15 19:24:17 +0600

    doing nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,12 @@ const Navbar = () => {
   return (
     <div className='flex py-3 flex-wrap justify-around'>
       <h1 className='text-lg font-semibold'>Todo App</h1>
-
+         <ul className='flex gap-[40px] font-medium'>
+           <li>Home</li>
+           <li>Products</li>
+           <li>About</li>
+           <li>Contact</li>
+         </ul>
     </div>
   )
 }
