fb405e8ef7352d73d39ce6dbc9ba9d98851d5f1a
Fatiha26 <f6lamia@gmail.com>
2024-10-15 19:37:18 +0600

    todo form

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,16 @@ import "./globals.css";
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Welcome to TODO app</h1>
-      </div>
+      <>
+        <form className="flex items-start flex-col gap-2 w-[80%] max-w-[600px]
+        mt-24 px-2 mx-auto">
+           <input type="text" name="title" placeholder="Enter Title"
+           className="px-3 py-2 border-2 w-full" />
+           <textarea name="description" placeholder="Enter description"
+           className="px-3 py-2 border-2 w-full"></textarea>
+           <button type="submit" className="bg-orange-600 py-3 px-11 text-white">Add Todo</button>
+        </form>
+      </>
     </>
   );
 }
